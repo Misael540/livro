c30eb8d9c397f5162b06600e605e8ecdea289334
Misael540 <68887244+Misael540@users.noreply.github.com>
2020-10-25 18:28:45 -0300

    Update

---
+++ public/js/script.js
@@ -0,0 +1,198 @@
+
+function listing(data){
+
+    var xhttp;
+  
+    if (window.XMLHttpRequest) {
+      xhttp = new XMLHttpRequest();
+    }else{
+      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  
+    }
+    xhttp.onreadystatechange = function() {
+     if (this.readyState == 4 && this.status == 200) {
+       
+       if(this.responseText){
+        $("#tbody1").html(this.responseText);
+       }
+      }
+    };
+  
+    if(data){
+      xhttp.open("GET", "data?action=list&search=" + data, true);
+  
+    }else{
+      xhttp.open("GET", "data?action=list", true);
+  
+    }
+  
+    xhttp.send();
+  }
+  
+  function get(data){
+    
+    if (window.XMLHttpRequest) {
+      xhttp = new XMLHttpRequest();
+    }else{
+      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  
+    }
+  
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if(this.responseText == 'success'){
+          document.getElementById("spanreturn1").style.color = "#60d25c";
+          $("#spanreturn1").text("Livro cadastrado com sucesso!");
+          document.getElementById("nome").value = ""; 
+          document.getElementById("categoria").value = ""; 
+          document.getElementById("imagem").value = ""; 
+          document.getElementById("autor").value = ""; 
+          document.getElementById("tipo").value = ""; 
+          document.getElementById("status").value = "";
+        
+        }else{
+          document.getElementById("spanreturn1").style.color = "#fd6f6f";
+          $("#spanreturn1").text("Erro, verifique os dados e tente novamente!");
+  
+        }
+  
+      }
+    };
+  
+    xhttp.open("GET", "data?" + data, true);
+    xhttp.send();
+  
+  }
+  
+  function add(int){
+  
+    $("#spanreturn1").text("");
+  
+    document.getElementById("edit").className = 'divFlutuanteHidden';
+    document.getElementById("del").className = 'divFlutuanteHidden';
+  
+    if(int == 1){
+      var nome = document.getElementById("nome").value; 
+      var categoria = document.getElementById("categoria").value; 
+      var imagem = document.getElementById("imagem").value; 
+      var autor = document.getElementById("autor").value; 
+      var tipo = document.getElementById("tipo").value; 
+      var status = document.getElementById("status").value;
+  
+      if(nome && autor && status){
+  
+        get("action=addition&nome="+nome+"&categoria="+categoria+"&imagem="+imagem+"&autor="+autor+"&tipo="+tipo+"&status="+status);   
+        listing();
+  
+      }else{
+        $("#spanreturn1").text("Preencha os campos obrigatórios (*) !");
+        document.getElementById("spanreturn1").style.color = "#fd6f6f";
+      }
+  
+    }else{
+    document.getElementById("add").className = 'divFlutuante';
+    
+    }
+  }
+  
+  function edit(id, save){
+    
+    document.getElementById("add").className = 'divFlutuanteHidden';
+    document.getElementById("del").className = 'divFlutuanteHidden';
+    document.getElementById("edit").className = 'divFlutuante';
+    $("#spanreturn2").text("");
+  
+    var xhttp;
+  
+    if (window.XMLHttpRequest) {
+      xhttp = new XMLHttpRequest();
+    }else{
+      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+  
+    if(save){
+  
+      var nome = document.getElementById("nome-edit").value; 
+      var categoria = document.getElementById("categoria-edit").value; 
+      var imagem = document.getElementById("imagem-edit").value; 
+      var autor = document.getElementById("autor-edit").value; 
+      var tipo = document.getElementById("tipo-edit").value; 
+      var status = document.getElementById("status-edit").value;
+  
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          if(this.responseText == "sucess"){
+            document.getElementById("spanreturn2").style.color = "#60d25c";
+            $("#spanreturn2").text("Salvo com sucesso!");
+  
+          }else{
+            document.getElementById("spanreturn2").style.color = "#fd6f6f";
+            $("#spanreturn2").text("Erro ao salvar.");
+          }
+        }
+      };
+  
+      xhttp.open("GET", "data?action=edit&id="+id+"&nome="+nome+"&categoria="+categoria+"&imagem="+imagem+"&autor="+autor+"&tipo="+tipo+"&status="+status, true);
+      xhttp.send();
+      
+    }else{
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          if(this.responseText){
+            $("#edit").html(this.responseText);
+          }
+        }
+    };
+  
+      xhttp.open("GET", "data?action=list&id=" + id, true);
+      xhttp.send();
+  
+    }
+  
+    listing();
+  
+  }
+  
+  function Poupdel(id){
+  
+    document.getElementById("add").className = 'divFlutuanteHidden';
+    document.getElementById("edit").className = 'divFlutuanteHidden';
+    document.getElementById("del").className = 'divFlutuante';
+    document.getElementById("id").value = id;
+  
+  }
+  
+  function del(){
+  
+    if (window.XMLHttpRequest) {
+      xhttp = new XMLHttpRequest();
+    }else{
+      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    
+    id = document.getElementById("id").value;
+  
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          if(this.responseText == "sucess"){
+            voltar();
+            listing();
+          }else{
+  
+          }
+        }
+      };
+  
+      xhttp.open("GET", "data?action=del&id=" + id, true);
+      xhttp.send();
+  
+  }
+  
+  
+  function voltar(){
+    document.getElementById("add").className = 'divFlutuanteHidden';
+    document.getElementById("edit").className = 'divFlutuanteHidden';
+    document.getElementById("del").className = 'divFlutuanteHidden';
+  
+  }
+  
--- resources/views/site/database.blade.php
+++ resources/views/site/database.blade.php
@@ -1,6 +1,17 @@
 <?php
 error_reporting(0);
 
+if($_COOKIE["hash"] && $_COOKIE['user']){
+
+    $link = mysqli_connect("127.0.0.1", "root", "", "livros");
+
+  
+
+}else{
+    header('Location: ./login');
+    die();
+}
+
 $link = mysqli_connect("127.0.0.1", "root", "", "livros");
 
 $id = $_GET['id'];
